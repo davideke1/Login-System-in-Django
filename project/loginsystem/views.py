9de36b132f4c50b6642c24b3e69a3ebfb10de7a1
davideke1 <ekechukwudavid@gmail.com>
2022-07-27 11:12:30 +0530

    signup, signin view and signin html page

--- project/loginsystem/views.py
+++ project/loginsystem/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import render
-from django.http    import HttpResponse
-
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, "loginsystem/index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['firstname']
+        lname = request.POST['lastname']
+        email = request.POST['email']
+        pass1 = request.POST['password']
+        pass2 = request.POST['confirmpassword']
+
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
     return render(request, "loginsystem/signup.html")
 
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['password']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "loginsystem/index.html", {'fname':fname})
+
+        else:
+            messages.error(request, "Invalid Password or username")
+            return redirect('home')
+
     return render(request, "loginsystem/signin.html")
 
 def signout(request):
